04ca61ce68a68d1f2e2912aee4f8f55468840fc2
gotankersley <gotankersley@harding.edu>
2014-09-05 23:57:55 -0500

    Organizing gui better

--- js/gui.js
+++ js/gui.js
@@ -4,21 +4,35 @@ var GUI = {
 	'Shadows': true,
 	'Popping cans': false,
 	'Zero gravity': false,
-	'Floor texture': true
+	'Floor texture': true	
 };
 
-window.onload = function() { 
-  var gui = new dat.GUI();
-  gui.add(GUI, 'Can rows').min(1).max(10).step(1);
-  gui.add(GUI, 'Update');
-  gui.add(GUI, 'Shadows').onChange(function() {
-	renderer.shadowMapAutoUpdate = GUI['Shadows'];
-	if (!GUI['Shadows']) renderer.clearTarget( light.shadowMap );	
-  });
-  
-  gui.add(GUI, 'Popping cans');
-  gui.add(GUI, 'Zero gravity');
-  gui.add(GUI, 'Floor texture').onChange(function() {
-	floor.material = GUI['Floor texture']? floorText : floorUntext;		
-  });
+window.onload = function() {
+	var gui = new dat.GUI();
+	//Physics
+	var folderPhysics = gui.addFolder('Physics');
+	folderPhysics.add(GUI, 'Popping cans');
+	folderPhysics.add(GUI, 'Zero gravity');
+	
+	//Soda types
+	var folderTypes = gui.addFolder('Soda types');
+	for (var i = 0; i < textures.length; i++) {
+		var text = textures[i];
+		GUI[text] = true;
+		folderTypes.add(GUI, text);
+	}	
+	
+	//Rendering options
+	var folderRender = gui.addFolder('Rendering options');
+	folderRender.add(GUI, 'Shadows').onChange(function() {
+		renderer.shadowMapAutoUpdate = GUI['Shadows'];
+		if (!GUI['Shadows']) renderer.clearTarget( light.shadowMap );	
+	});
+	folderRender.add(GUI, 'Floor texture').onChange(function() {
+		floor.material = GUI['Floor texture']? floorText : floorUntext;		
+	});
+	
+	
+	gui.add(GUI, 'Can rows').min(1).max(10).step(1);
+	gui.add(GUI, 'Update');
 }
